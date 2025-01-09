a95abb954d17bbcc68c36fe325f79d68c3638e2b
Tayling <taymng@yahoo.com.br>
2025-01-09 09:10:25 -0300

    feat: Section 2 Structure

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,20 @@
-export const HomePage = () => {
-  return <h1 className="text-2xl font-extrabold text-zinc-700">Home</h1>;
+// export const HomePage = () => {
+//   return <h1 className="text-2xl font-extrabold text-zinc-700">Home</h1>;
+// };
+
+// import React from 'react';
+// import Header from '../components/Header';
+// import HomeSec2 from './components/HomePage/HomeSec2';
+// import Footer from '../components/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            {/* <Header /> */}
+            {/* <HomeSec2 /> */}
+            {/* <Footer /> */}
+        </div>
+    );
 };
+
+export default Home;
