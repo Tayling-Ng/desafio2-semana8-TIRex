9027fa9db42396e824f5cc13b6e511c90f2524d1
Gabriel-GGC <gabrielcrestani@hotmail.com>
2025-01-09 15:42:29 -0300

    feat: Update Tailwind CSS configuration with custom font and dimension extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: { roboto: ['Roboto', 'sans-serif'] },
+            height: { 
+                '119.89px': '119.89px',
+                '181px': '181px',
+                '17px': '17px',
+                '109px': '109px',
+                '417px': '417px',
+                '108px': '108px',
+                '300.5px': '300.5px',
+            
+             },
+            width: { 
+                '115.36px': '115.36px', 
+                '174.16px': '174.16px',
+                '401px': '401px',
+                '410px': '410px',
+                '21.84px': '21.84px',
+                '375px': '375px',
+                
+            },
+        }
+
     },
     plugins: [],
 };
