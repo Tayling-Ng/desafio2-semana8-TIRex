36e6077c2f910064d4805321c22c3d78077bd4dc
Tayling <taymng@yahoo.com.br>
2025-01-10 17:09:03 -0300

    feat: start structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { PageRoute } from './pages/page-route';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <BrowserRouter>
+            <PageRoute />
+        </BrowserRouter>
+    </StrictMode>
+);
