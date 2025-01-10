3f426bcbebe1416e0463c569528f9485a9612c34
Paula Borges <paulabzambeli@gmail.com>
2025-01-10 17:37:38 -0300

    feat:haeder-haeder-layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { PageRoute } from './pages/page-route';
+import Header from './components/Header/Header'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
+        <Header />
             <PageRoute />
         </BrowserRouter>
     </StrictMode>
