c5d874e975a4f24b17aa8d70aef3eca9f06cf726
Thiago <tgomes_2@hotmail.com>
2025-01-10 12:56:35 -0300

    feat: add section3 for desktop layout

--- src/components/HomePage/Section3/IconCard.tsx
+++ src/components/HomePage/Section3/IconCard.tsx
@@ -6,11 +6,11 @@ interface IconCardProps {
 
 function IconCard({url, title, subtitle}: IconCardProps){
     return(
-        <div className="flex flex-col justify-center items-center gap-[20px] p-[12px] w-[353px] h-[204px]">
+        <div className="flex flex-col justify-center items-center gap-[20px] p-[12px] w-[353px] h-[204px] sm:w-[229px] xl:w-[245px]">
             <div className="bg-white w-[100px] h-[100px] rounded-[50%] flex justify-center items-center">
                 <img src={url} alt="Icon" className="w-[48px] h-[48px] " />
             </div>
-            <div className="flex flex-col gap-[8px]">
+            <div className="flex flex-col gap-[8px] sm:w-[229px] xl:w-[245px]">
                 <p className="text-white text-[20px] leading-[28px] text-center">{title}</p>
                 <p className="text-white text-[16px] leading-[24px] text-center">{subtitle}</p>
             </div>
