cf1772a16271528094a1756d1020a8d05ec1501d
Tayling <taymng@yahoo.com.br>
2025-01-11 18:20:12 -0300

    feat: section 4 mobile-first layout

--- src/components/HomePage/Section4/Section4 .tsx
+++ src/components/HomePage/Section4/Section4 .tsx
@@ -1,35 +1,44 @@
-const HomeSec4 = () => {
+const Section4 = () => {
     return (
-        <section className="flex flex-col items-center w-[393px] h-[510px] py-[60px] bg-blue-300">
+        <section className="flex flex-col justify-center items-center w-[393px] h-[510px] py-[60px] bg-white gap-[60px]">
             <div className="flex flex-col items-center gap-[24px]">
-                <h1 className="">Subscribe to Update</h1>
-                <p className="">Stay informed with our lates updates</p>
+                <h1 className="text-[25px] leading-[48px] text-[#160A60] font-bold">
+                    Subscribe to Update
+                </h1>
+                <p className="text-[14px] leading-[24px] text-[#160A60]">
+                    Stay informed with our lates updates
+                </p>
             </div>
-            <div>
+            <div className="flex flex-col w-[353px] h-[256px] gap-10">
                 <form className="">
-                    <label htmlFor="email" className="">
+                    <label htmlFor="email" className="text-[#160A60] font-bold">
                         Email
                     </label>
                     <input
+                        className="w-[353px] h-[36px] rounded-[6px] border border-black border-opacity-50 px-3"
                         type="email"
                         id="email"
                         placeholder="Enter your email"
-                        className=""
                     />
-                    <div className="">We promise not to spam</div>
+                    <div className=" text-black opacity-50">
+                        We promise not to spam
+                    </div>
                 </form>
                 <form className="">
-                    <label htmlFor="name" className="">
+                    <label htmlFor="name" className="text-[#160A60] font-bold">
                         Name
                     </label>
                     <input
+                        className="w-[353px] h-[36px] rounded-[6px] border border-black border-opacity-50 px-3"
                         type="password"
                         id="password"
                         placeholder="Enter your name"
-                        className=""
                     />
                 </form>
-                <button className="bg-[#1E293B] text-white" type="submit">
+                <button
+                    className="flex justify-center items-center w-[148px] h-[36px] p-[12px] font-medium text-[15px] leading-[24px] rounded-[8px] bg-[#1E293B] text-white self-center"
+                    type="submit"
+                >
                     Subscribe
                 </button>
             </div>
@@ -37,4 +46,4 @@ const HomeSec4 = () => {
     );
 };
 
-export default HomeSec4;
+export default Section4;
