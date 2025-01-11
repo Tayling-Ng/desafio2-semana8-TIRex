fababaabd8abe0611e304c3a45d6f36583beb788
Paula Borges <paulabzambeli@gmail.com>
2025-01-11 12:38:59 -0300

    fix:componente-Pagenotfound-img

--- src/components/PageNotFound/PageNotFoundComponet.tsx
+++ src/components/PageNotFound/PageNotFoundComponet.tsx
@@ -3,11 +3,11 @@ import ImgNotFound from '../../assets/page-not-found/ImgNotFound.svg'
 
 const PageNotFoundComponent = () => {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen gap-5'>
+    <div className='flex flex-col items-center justify-center min-h-screen'>
       <div className="flex justify-center">
-        <img src={ImgNotFound} alt="Image Not Found" className="w-[300px] h-[200px]" />
+        <img src={ImgNotFound} alt="Image Not Found" className="min-w-[300px] min-h-[200px]" />
       </div>
-      <div className='flex flex-col items-center min-w-[230px] min-h-[196px] gap-4'>
+      <div className='flex flex-col items-center min-w-[230px] min-h-[196px]'>
         <h1 className='text-[86px] font-bold leading-[36px] text-center'>404</h1>
         <p className='text-[18px]  leading-[36px] text-center '>Oops! Nothing to see here, just a lost kanban task.</p>
         <Link to="/" className=" text-[#4F46E5] text-[15px] leading-[36px] text-end ">Go back to home</Link>
