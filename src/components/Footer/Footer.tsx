37a3b5637d137ffb96fa896808386de87de16e65
Paula Borges <paulabzambeli@gmail.com>
2025-01-11 18:16:46 -0300

    fix:componet footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,34 +2,34 @@ import instagram from '../../assets/img-header-footer/instagram.jpg'
 import linkedin from '../../assets/img-header-footer/linkedin.jpg'
 import facebook from '../../assets/img-header-footer/facebook.jpg'
 const Footer = () => {
-  return (
-    <div className=' flex flex-col items-center min-w-[393px] min-h-[206px] p-[20px] bg-[#1E293B] sm:flex justify-center sm:items-center
-    sm:min-w-[744px] sm:min-h-[145px] xl:flex-row xl:justify-center xl:items-center xl:min-w-[1440px] xl:min-h-[100px] xl:gap-[60px]'>
-        <div className='flex justify-between w-[136px] -h-[34.5px] '>
-            <a href="https://www.facebook.com/">
-                <div className='icon-bg-white flex justify-center items-center w-[34px] h-[34px] bg-white rounded-full '> 
-                    <img src={facebook} alt="icon Facebook" className='w-[11px] h-[11px]' />
-                </div>
-            </a>
-            <a href="https://www.instagram.com/">
-                <div className='icon-bg-white flex justify-center items-center w-[34px] h-[34px] bg-white rounded-full '> 
-                    <img src={instagram} alt="icon intagram" className='w-[11px] h-[11px]' />
-                </div>
-            </a>
-            <a href="https://www.linkedin.com/">
-                <div className='icon-bg-white flex justify-center items-center w-[34px] h-[34px] bg-white rounded-full '> 
-                    <img src={linkedin} alt="icon linkedin" className='w-[11px] h-[11px]' />
-                </div>
-            </a>
-        </div>
-        <div className='flex flex-col text-center text-white text-[14px] leading-[28px] gap-[5px] p-[30px]  sm:text-[18px] xl:flex-row xl:justify-around  xl:gap-[60px]'>
-            <p>&copy; 2024 Project Manager.All rights reserved.</p>
-            <p>Privacy Policy</p>
-            <p>Terms of Service</p>
+    return (
+        <div className=' flex flex-col items-center w-full h-[206px]  bg-[#1E293B]  justify-center py-[20px] gap-[20px] tablet:px-[19px] 
+        tablet:h-[145px] desktop:h-[100px] desktop:flex-row desktop:gap-[60px]'>
+            <div className='flex gap-[16px] ' >
+                <a href="https://www.facebook.com/" className='icon-bg-white flex justify-center items-center w-[34px] h-[34px] bg-white rounded-full ' >
 
-        </div> 
-    </div>
-  )
+                    <img src={facebook} alt="icon Facebook" className='w-[11px] h-[11px]' />
+
+                </a>
+                <a href="https://www.instagram.com/" className='icon-bg-white flex justify-center items-center w-[34px] h-[34px] bg-white rounded-full ' >
+
+                    <img src={instagram} alt="icon intagram" className='w-[11px] h-[11px]' />
+
+                </a>
+                <a href="https://www.linkedin.com/" className='icon-bg-white flex justify-center items-center w-[34px] h-[34px] bg-white rounded-full '>
+
+                    <img src={linkedin} alt="icon linkedin" className='w-[11px] h-[11px]' />
+
+                </a>
+            </div>
+            <div className='flex flex-col items-center justify-center text-white  gap-[20px] tablet:flex-row font-normal  '>
+                <p className='tablet:text-[18px]  text-[14px] leading-[28px] desktop:text-[20px]'>&copy; 2024 Project Manager.All rights reserved.</p>
+                <p className='tablet:text-[18px]  text-[14px] leading-[28px] desktop:text-[20px]'>Privacy Policy</p>
+                <p className='tablet:text-[18px]  text-[14px] leading-[28px] desktop:text-[20px]' >Terms of Service</p>
+
+            </div>
+        </div>
+    )
 }
 
 export default Footer
