4c4c4ba088739a6128b9f7933bff957f0f791310
Paula Borges <paulabzambeli@gmail.com>
2025-01-11 21:03:33 -0300

    fix:componente-header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,60 @@
-import { useLocation } from 'react-router-dom'
-import logo from '../../assets/img-header-footer/logo.svg'
-import { Buttom } from './Buttom'
-import NavBar from './NavBar'
+import { useLocation } from "react-router-dom";
+import logo from "../../assets/img-header-footer/logo.svg";
+import NavBarMobile from "./NavBarMobile";
+import menuIcon from "../../assets/img-header-footer/menu.svg";
+import shapeIcon from "../../assets/img-header-footer/shape.svg";
+import { useState } from "react";
+import NavBarTabletDesktop from "./NavBarTabletDesktop";
 
 const Header = () => {
-  const location = useLocation()
+  const location = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
   console.log(location.pathname);
 
   return (
-    <div className='flex justify-between items-center min-w-[393px] min-h-[80px] bg-[#1E293B] text-white p-[20px] 
-    sm:min-w-[744px] sm:min-h-[80px] 
-    xl:min-w-[1440px] xl:min-h-[80px]'>
-
-      <div className='flex items-center gap-[20px]'>
-        <img src={logo} alt="Logo" 
-        className='w-[40px] h-[40px] rounded-[50px]
-         sm:w-[50px] sm:h-[50px] 
-         xl:w-[50px] xl:h-[50px]' />
-        <p className='text-[16px] leading-[36px] 
-        sm:text-[18px] 
-        xl:text-[28px]'>Project Manager</p>
+    <div className="flex flex-col tablet:flex-row w-full h-fit bg-[#1E293B] text-white px-[20px] py-[15px]">
+      <div className="flex w-full justify-between items-center">
+        <div className="flex items-center gap-[20px]">
+          <img src={logo} alt="Logo" className="w-[50px] h-[50px]" />
+          <p className="text-[16px] leading-[36px] sm:text-[18px] xl:text-[28px]">
+            Project Manager
+          </p>
+        </div>
+        {location.pathname === "/" && (
+          <a
+            href="/login"
+            className="w-[105px] h-[36px] text-[15px] leading-[24px] rounded-[30px] border flex justify-center items-center border-white tablet:text-[16px] tablet:w-[162px] tablet:h-[48px] desktop:w-[254px] desktop:h-[60px]"
+          >
+            Login
+          </a>
+        )}
+        {location.pathname.includes("kanban") && (
+          <div className="tablet:hidden flex gap-[20px] items-center">
+            <button
+              onClick={() => {
+                console.log("setMenuOpen(!menuOpen)");
+                setMenuOpen(!menuOpen);
+              }}
+            >
+              <img src={menuIcon} alt="Menu" className="w-[24px] h-[24px]" />
+            </button>
+            <div
+              className={`${menuOpen ? "hidden" : ""
+                } h-[36px] w-[36px] flex items-center justify-center rounded  border-[#FFFFFF1A] border text-[#FFFFFF80]`}
+            >
+              <img src={shapeIcon} alt="Search" className="w-[16px] h-[16px]" />
+            </div>
+          </div>
+        )}
       </div>
-      {location.pathname === '/' && <Buttom/>}
-      {location.pathname.includes('kanban')  && <NavBar />}
-
+      {location.pathname.includes("kanban") && (
+        <>
+          <NavBarTabletDesktop />
+          <NavBarMobile menuOpen={menuOpen} />
+        </>
+      )}
     </div>
   );
-}
+};
 export default Header;
+
