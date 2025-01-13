ccb603abca1d4555b27dfd300254f1ed70684315
Heberton Geovane <heberton.geovane@gmail.com>
2025-01-13 11:22:16 -0300

    fix: tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,25 @@
 //     },
 //     plugins: [],
 // };
-
+ 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
+        screens: {
+            mobile:'393px',
+            tablet:'744px',
+            desktop:'1440px'
+ 
+        },
         extend: {
             fontFamily: {
                 sans: ['Roboto', 'sans-serif'],
+            },
+            
+            colors: {
+                'dark-brown': '#23081C',
+                'span-light-blue': '#92BBFF'
             },
         },
     },
