892a17da41295c506bb3c01173e39526acba5d6b
Gabriel-GGC <gabrielcrestani@hotmail.com>
2025-01-13 15:59:15 -0300

    feat(tailwindconfig): add screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
+        screens: {
+            mobile:'393px',
+            tablet:'744px',
+            desktop:'1440px'
+
+        },
         extend: {
             fontFamily: { roboto: ['Roboto', 'sans-serif'] },
             height: { 
