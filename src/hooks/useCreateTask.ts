381772e0fa848cea8a305ed00298b6069f5aa073
paulagmborges <paulabzambeli@gmail.com>
2025-01-14 18:53:18 -0300

    feat:json-integrated-profile

--- src/hooks/useCreateTask.ts
+++ src/hooks/useCreateTask.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
-import { useCallback, useState } from "react"
- 
- 
+import { useState } from "react"
+
+
 interface ICreateTask {
     priority: string
     title: string
@@ -13,33 +13,32 @@ interface ICreateTask {
     timeEstimate: string
     createdBy: string
 }
- 
- 
+
+
 const useCreateTask = () => {
- 
- 
-    const [ isSubmit, setIsSubmit ] = useState(false)
- 
- 
-    const createTask = useCallback(async (task: ICreateTask) => {
- 
+
+
+    const [isSubmit, setIsSubmit] = useState(false)
+
+
+    const createTask = async (task: ICreateTask) => {
+
         setIsSubmit(true)
- 
+
         console.log('valores recebidos', task)
         const id = Math.random().toString(36).substring(2, 15)
         const response = await axios.post('http://localhost:3000/tasks', { id, ...task })
         console.log('deu certo a criação no db.json', response.data)
- 
+
         setIsSubmit(false)
-    }, [])
- 
- 
+    }
+
+
     return {
         createTask,
         isSubmit
     }
 }
- 
+
 export default useCreateTask
- 
- 
+
